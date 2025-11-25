14045442c6f7e0297ae4abbe2e2e12ea40fd53c8
ReubenKR <reuben.robbings@gmail.com>
2025-11-25 09:47:52 -0800

    Update Shapes.java

--- Shapes.java
+++ Shapes.java
@@ -1,14 +1,34 @@
 public class Shapes {
+   
     public static void main(String[] args) {
+         int turnNum = 0;
         // Task 1: Draw a regular shape
-
+         Turtle turtle = new Turtle();
+         for (int i = 0; i < 4; i++) { 
+         turtle.forward(100);
+         turtle.right(90);
+         turnNum++;
+         }
 
         // Task 2: Draw a regular shape many times
+        
+         Turtle yurtle = new Turtle();
+         
+         for (int i = 0; i < 6; i++) {
+            for (int l = 0; l < 4; l++) {
+               
+               yurtle.forward(100);
+               yurtle.right(90);
+               turnNum++;
+            }
+            yurtle.right(60);
+            turnNum++;
+         }
 
 
         // Task 3: How many times does Turtle turn!
-
-
+         System.out.print("Turtle turns " + turnNum + " times!");
+         
         // Extension 1:
 
 
